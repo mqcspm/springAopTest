5e4e78a9cb07db4c5be0e13bb0e2a7caedccc994
mengqingcai <mqc_spm@163.com>
2018-03-30 10:38:54 +0800

    新增通知类型

--- aopTest/src/main/java/com/meng/asp/Intercept.java
+++ aopTest/src/main/java/com/meng/asp/Intercept.java
@@ -13,14 +13,47 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class Intercept {
+
+    /**
+     * 设置切入点
+     */
     @Pointcut(value = "@annotation(com.meng.anno.Say)")
     public void point(){
     }
 
+    /**
+     * 切入点方法执行后,获取执行方法的返回值
+     * @param result
+     * @throws Throwable
+     */
     @AfterReturning(value = "point()", returning = "result")
-    public void doAfter(Object result) throws Throwable {
+    public void doAfterReturning(Object result) throws Throwable {
         Person person = (Person) result;
         person.setName("二狗子");
         System.out.println("****** AfterReturning" + result.toString());
+    }
+
+    /**
+     * 带有自定义注解方法执行之前,执行切入方法
+     */
+    @Before("point()")
+    public void doBefore(){
+
+    }
+
+    /**
+     * 带有自定义注解的方法执行后,执行切入方法
+     */
+    @After("point()")
+    public void doAfter(){
+
+    }
+
+    /**
+     * 环绕通知类型,带有自定义注解方法,执行前、执行后,都会执行切入方法
+     */
+    @Around("point()")
+    public void doAround(){
+
     }
 }
